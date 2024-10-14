ed6c3d2c407711bc01d4f42b5612a10fb6fb83e2
Zheli Chen <zhelichen@ucsb.edu>
2024-10-14 16:35:21 -0700

    nav bar

---
+++ ucsb-bathrooms/ucsb-bathrooms/TabBarView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct TabBarView: View {
+
+    @State private var selectedTab = 0
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+
+            ContentView()
+                .tabItem {
+                    if self.selectedTab == 0 {
+                        Image(systemName: "map")
+                    } else {
+                        Image(systemName: "map.fill")
+                    }
+                    Text("Map")
+                }
+                .tag(0)
+            
+            ContentView()
+                .tabItem {
+                    if self.selectedTab == 1 {
+                        Image(systemName: "star")
+                    } else {
+                        Image(systemName: "star.fill")
+                    }
+                    Text("placeholder")
+                }
+                .tag(1)
+
+            ContentView()
+                .tabItem {
+                    if self.selectedTab == 1 {
+                        Image(systemName: "person")
+                    } else {
+                        Image(systemName: "person.fill")
+                    }
+                    Text("My Account")
+                }
+                .tag(2)
+        }
+    }
+}
+
+#Preview {
+    TabBarView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
